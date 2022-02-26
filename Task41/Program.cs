7a77d270c4ae68584c6bb113e1ab18bc63e61911
Max <lmaxvv@gmail.com>
2022-02-26 13:19:55 +0900

    Задача 41 Triangle

---
+++ Task41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+
+
+bool IsTriangle(double x, double y, double z)
+{
+    return (x + y > z && x + z > y && y + z > x);
+}
+
+double a = 1, b = 4, c = 7;
+
+//(условие)? вернуть если истина : вернуть если ложь
+System.Console.WriteLine($"{a} {b} {c} - " + (IsTriangle(a, b, c)?"Это стороны треугольника":"Это не стороны треугольника"));
