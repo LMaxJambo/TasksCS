f6a31a7caa57cd7a30790a300a1dd3d5ac6dbf80
Max <lmaxvv@gmail.com>
2022-03-07 23:42:08 +0900

    task 49

---
+++ Task49/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] a = new double[2, 2];
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        a[i, j] = new Random().NextDouble() * 100;
+        System.Console.Write("a[{0},{1}]={2,6:F2} ", i, j, a[i, j]);
+    }
+    System.Console.WriteLine();
+}
